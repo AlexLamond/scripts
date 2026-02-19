74f116469c074bbd18416840ae74782e9c381aa4
Alex L <9060360+AlexLamond@users.noreply.github.com>
2026-02-19 08:42:15 +0100

    Deploy Azure Local Node Script

---
+++ Deploy_AzureLocal_Node.ps1
@@ -0,0 +1,57 @@
+# This guide assumes you already have the OS installed, and access to an ADDS
+
+# Ensure LCM User is created inside your AD
+
+Install-Module AsHciADArtifactsPreCreationTool -Repository PSGallery -Force
+$password = ConvertTo-SecureString '<Password>' -AsPlainText -Force
+$user = "<LCMUser>"
+$credential = New-Object System.Management.Automation.PSCredential ($user, $password)
+New-HciAdObjectsPreCreation -AzureStackLCMUserCredential $credential -AsHciOUName "OU=<OU>,DC=<DC>,DC=<DC>"
+
+#Enable RDP inside the OS, and install network drivers
+
+Get-Volume
+cd <volume>
+./SetupBD.exe # / The file your driver installer is
+
+# Ensure the driver shows the correct manufacturer, and not Microsoft
+
+Get-NetAdapter | fl *driver*
+
+# Set you management addresses to static and setup DNS
+
+$NewPassword = ConvertTo-SecureString "<SecurePassword>" -AsPlainText -Force
+Set-LocalUser -Name "Administrator" -Password $NewPassword
+
+Rename-Computer -NewName "<NodeName>"
+
+# Restart the host
+
+# Cleanup the disks on the host
+
+$ServerList = "<NodeName>"
+ 
+Invoke-Command ($ServerList) {
+Update-StorageProviderCache
+Get-StoragePool | ? IsPrimordial -eq $false | Set-StoragePool -IsReadOnly:$false -ErrorAction SilentlyContinue
+      Get-StoragePool | ? IsPrimordial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -Confirm:$false -ErrorAction SilentlyContinue
+      Get-StoragePool | ? IsPrimordial -eq $false | Remove-StoragePool -Confirm:$false -ErrorAction SilentlyContinue
+      Get-PhysicalDisk | Reset-PhysicalDisk -ErrorAction SilentlyContinue
+      Get-Disk | ? Number -ne $null | ? IsBoot -ne $true | ? IsSystem -ne $true | ? PartitionStyle -ne RAW | ? BusType -ne USB | % {
+$_ | Set-Disk -isoffline:$false
+           $_ | Set-Disk -isreadonly:$false
+$_ | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
+           $_ | Set-Disk -isreadonly:$true
+           $_ | Set-Disk -isoffline:$true
+      }
+      Get-Disk | Where Number -Ne $Null | Where IsBoot -Ne $True | Where IsSystem -Ne $True | Where PartitionStyle -Eq RAW | Group -NoElement -Property FriendlyName
+} | Sort -Property PsComputerName, Count
+
+# Register the host with Azure
+
+$Tenant = "<Tenant>"
+$Subscription = "<Subscription>"
+$RG = "<ResourceGroup>"
+$Region = "<Region>"
+
+Invoke-AzStackHciArcInitialization -TenantId $Tenant -SubscriptionID $Subscription -ResourceGroup $RG -Region $Region -Cloud "AzureCloud"
