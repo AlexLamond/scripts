7b014bd0c65358f5554bc781bfbcb12556e13e57
Alex L <9060360+AlexLamond@users.noreply.github.com>
2026-02-25 10:04:37 +0100

    Clarify swapfile

--- Linux/ExpandSwapFile.sh
+++ Linux/ExpandSwapFile.sh
@@ -1,3 +1,5 @@
+# This will get the swap location, you must replace `/swapfile` with whatever is output here
+swapon -s
 sudo swapoff /swapfile
 sudo fallocate -l 2G /swapfile
 sudo mkswap /swapfile
